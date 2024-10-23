08a66b661176170206d98fe6e261cc89e5b092fc
Victor Prokhorov <mail@victorprokhorov.com>
2024-10-23 18:50:41 +0200

    iter1: init

---
+++ README.md
@@ -0,0 +1,9 @@
+# exploring networking basics
+
+## iteration 1: reference
+
+- https://www.raspberrypi.com/documentation/computers/remote-access.html#copy-files-to-your-raspberry-pi
+```sh
+scp myfile.txt <username>@<pi_ip_address>:
+```
+- `curl --trace curl_trace_iteration1_1.txt http://localhost:80` save trace of reference implementation
---
+++ iteration1/src/main.rs
@@ -0,0 +1,28 @@
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    let mut counter = 0;
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        counter += 1;
+        let mut buffer = [0; 1024];
+        let bytes_read = stream.read(&mut buffer).unwrap();
+        let request = String::from_utf8(buffer[..bytes_read].to_vec()).unwrap();
+        assert_eq!(
+            request.lines().next().unwrap(),
+            "GET / HTTP/1.1",
+            "can respond only to get request in http 1.1"
+        );
+        let body = counter.to_string();
+        let content_length = body.len();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{body}",
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
